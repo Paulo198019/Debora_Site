1ac68182c0a0909328420ae9092edbd6fb9a35e8
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2025-11-20 01:53:25 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function handleClick(action) {
-  if (action === 'saiba') {
-    alert('Você clicou em Saiba Mais!');
-  } else if (action === 'contato') {
-    alert('Você clicou em Contato!');
-  }
-}
