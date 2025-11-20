e701791562619e3173c7a2e2198ad41bb03f7eb6
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2025-11-20 01:47:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function handleClick(action) {
+  if (action === 'saiba') {
+    alert('Você clicou em Saiba Mais!');
+  } else if (action === 'contato') {
+    alert('Você clicou em Contato!');
+  }
+}
